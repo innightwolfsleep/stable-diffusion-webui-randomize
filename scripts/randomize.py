9c0c409782e6c4bd88f34494f0881382e4a4c4f2
MMaker <mmaker@mmaker.moe>
2022-11-12 08:49:17 -0500

    feat: Move params out of options
    This became an annoyance since settings can be frozen
    with the `--freeze-settings` flag. Should prevent cluttering
    up the UI as well.

--- scripts/randomize.py
+++ scripts/randomize.py
@@ -1,11 +1,11 @@
 import random
+import gradio as gr
 
-from modules import script_callbacks, scripts, shared
+from modules import scripts
 from modules.processing import (StableDiffusionProcessing,
                                 StableDiffusionProcessingTxt2Img)
 from modules.shared import opts
 from scripts.xy_grid import build_samplers_dict
-
 
 class RandomizeScript(scripts.Script):
 	def title(self):
@@ -14,14 +14,34 @@ class RandomizeScript(scripts.Script):
 	def show(self, is_img2img):
 		return scripts.AlwaysVisible
 
-	def process(self, p: StableDiffusionProcessing):
-		if shared.opts.randomize_enabled and isinstance(p, StableDiffusionProcessingTxt2Img):
-			all_opts = list(vars(shared.opts)['data'].keys())
+	def ui(self, is_img2img):
+		randomize_enabled, randomize_param_sampler_index, randomize_param_cfg_scale, randomize_param_steps, randomize_param_width, randomize_param_height, randomize_hires, randomize_hires_denoising_strength, randomize_hires_width, randomize_hires_height, randomize_other_CLIP_stop_at_last_layers = self._create_ui()
+
+		return [randomize_enabled, randomize_param_sampler_index, randomize_param_cfg_scale, randomize_param_steps, randomize_param_width, randomize_param_height, randomize_hires, randomize_hires_denoising_strength, randomize_hires_width, randomize_hires_height, randomize_other_CLIP_stop_at_last_layers]
+
+	def process(
+		self,
+		p: StableDiffusionProcessing,
+		randomize_enabled: bool,
+		randomize_param_sampler_index: str,
+		randomize_param_cfg_scale: str,
+		randomize_param_steps: str,
+		randomize_param_width: str,
+		randomize_param_height: str,
+		randomize_hires: str,
+		randomize_hires_denoising_strength: str,
+		randomize_hires_width: str,
+		randomize_hires_height: str,
+		randomize_other_CLIP_stop_at_last_layers: str,
+	):
+		if randomize_enabled and isinstance(p, StableDiffusionProcessingTxt2Img):
+			# TODO (mmaker): Fix this jank. Don't do this.
+			all_opts = {k: v for k, v in locals().items() if k not in ['self', 'p', 'randomize_enabled']}
 
 			# Base params
-			for param in self._list_params(all_opts):
+			for param, val in self._list_params(all_opts):
 				try:
-					opt = self._opt(param, p)
+					opt = self._opt({param: val}, p)
 					if opt is not None:
 						setattr(p, param, opt)
 					else:
@@ -30,15 +50,15 @@ class RandomizeScript(scripts.Script):
 					print(f'Failed to randomize param `{param}` -- incorrect value?')
 
 			# Other params
-			for param in self._list_params(all_opts, prefix='randomize_other_'):
+			for param, val in self._list_params(all_opts, prefix='randomize_other_'):
 				if param == 'CLIP_stop_at_last_layers':
-					opts.data[param] = int(self._opt(param, p, prefix='randomize_other_')) # type: ignore
+					opts.data[param] = int(self._opt({param: val}, p)) # type: ignore
 
 			# Highres. fix params
-			if random.random() < float(shared.opts.randomize_hires or 0): # type: ignore
+			if random.random() < float(randomize_hires or 0):
 				try:
-					setattr(p, 'width', self._opt('width', p, 'randomize_hires_'))
-					setattr(p, 'height', self._opt('height', p, 'randomize_hires_'))
+					setattr(p, 'width', self._opt({'width': randomize_hires_width}, p))
+					setattr(p, 'height', self._opt({'height': randomize_hires_height}, p))
 
 					setattr(p, 'enable_hr', True)
 					setattr(p, 'firstphase_width', 0)
@@ -46,21 +66,23 @@ class RandomizeScript(scripts.Script):
 					setattr(p, 'truncate_x', 0)
 					setattr(p, 'truncate_y', 0)
 
-					setattr(p, 'denoising_strength', self._opt('denoising_strength', p, prefix='randomize_hires_'))
+					setattr(p, 'denoising_strength', self._opt({'denoising_strength': randomize_hires_denoising_strength}, p))
 				except (TypeError, IndexError):
 					print(f'Failed to utilize highres. fix -- incorrect value?')
 		else:
 			return
 
 	def _list_params(self, opts, prefix='randomize_param_'):
-		for param in [o for o in filter(lambda x: x.startswith(prefix), opts)]:
-				if len(getattr(shared.opts, param).strip()) > 0:
-					yield param.split(prefix)[1]
+		for k, v in opts.items():
+			if k.startswith(prefix):
+				yield k.replace(prefix,''), v
 
-	def _opt(self, opt, p, prefix='randomize_param_'):
-		opt_name = f'{prefix}{opt}'
-		opt_val: str = getattr(shared.opts, opt_name)
+	def _opt(self, opt, p):
+		opt_name = list(opt.keys())[0]
+		opt_val = list(opt.values())[0]
+
 		opt_arr: list[str] = [x.strip() for x in opt_val.split(',')]
+
 		if self._is_num(opt_arr[0]) and len(opt_arr) == 3:
 			vals = [float(v) for v in opt_arr]
 			rand = self._rand(vals[0], vals[1], vals[2])
@@ -69,7 +91,7 @@ class RandomizeScript(scripts.Script):
 			else:
 				return round(float(rand), max(0, int(opt_arr[2][::-1].find('.'))))
 		else:
-			if opt == 'sampler_index':
+			if opt_name == 'sampler_index':
 				return build_samplers_dict(p).get(random.choice(opt_arr).lower(), None)
 			else:
 				return None
@@ -87,17 +109,19 @@ class RandomizeScript(scripts.Script):
 			except ValueError:
 				return False
 
-def on_ui_settings():
-	shared.opts.add_option('randomize_enabled', shared.OptionInfo(False, 'Enable Randomize extension', section=('randomize', 'Randomize')))
-	shared.opts.add_option('randomize_param_sampler_index', shared.OptionInfo('euler a,euler', 'Randomize Sampler', section=('randomize', 'Randomize')))
-	shared.opts.add_option('randomize_param_cfg_scale', shared.OptionInfo('5,15,0.5', 'Randomize CFG Scale', section=('randomize', 'Randomize')))
-	shared.opts.add_option('randomize_param_steps', shared.OptionInfo('10,50,2', 'Randomize Steps', section=('randomize', 'Randomize')))
-	shared.opts.add_option('randomize_param_width', shared.OptionInfo('256,768,64', 'Randomize Width', section=('randomize', 'Randomize')))
-	shared.opts.add_option('randomize_param_height', shared.OptionInfo('256,768,64', 'Randomize Height', section=('randomize', 'Randomize')))
-	shared.opts.add_option('randomize_hires', shared.OptionInfo('0.25', 'Randomize Highres. percentage', section=('randomize', 'Randomize')))
-	shared.opts.add_option('randomize_hires_denoising_strength', shared.OptionInfo('0.5,0.8,0.05', 'Randomize Highres. Denoising Strength', section=('randomize', 'Randomize')))
-	shared.opts.add_option('randomize_hires_width', shared.OptionInfo('768,1920,64', 'Randomize Highres. Width', section=('randomize', 'Randomize')))
-	shared.opts.add_option('randomize_hires_height', shared.OptionInfo('768,1920,64', 'Randomize Highres. Height', section=('randomize', 'Randomize')))
-	shared.opts.add_option('randomize_other_CLIP_stop_at_last_layers', shared.OptionInfo('1,2,1', 'Randomize CLIP skip', section=('randomize', 'Randomize')))
-
-script_callbacks.on_ui_settings(on_ui_settings)
+	def _create_ui(self):
+		with gr.Group():
+			with gr.Accordion('Randomize', open=False):
+				randomize_enabled = gr.Checkbox(label='Enable', value=False)
+				randomize_param_sampler_index = gr.Textbox(label='Sampler', value='euler a, euler')
+				randomize_param_cfg_scale = gr.Textbox(label='CFG Scale', value='5, 15, 0.5')
+				randomize_param_steps = gr.Textbox(label='Steps', value='10, 50, 2')
+				randomize_param_width = gr.Textbox(label='Width', value='256, 768, 64')
+				randomize_param_height = gr.Textbox(label='Height', value='256, 768, 64')
+				randomize_hires = gr.Textbox(label='Highres. percentage chance', value='0.25')
+				randomize_hires_denoising_strength = gr.Textbox(label='Highres. Denoising Strength', value='0.5, 0.8, 0.05')
+				randomize_hires_width = gr.Textbox(label='Highres. Width', value='768, 1920, 64')
+				randomize_hires_height = gr.Textbox(label='Highres. Height', value='768, 1920, 64')
+				randomize_other_CLIP_stop_at_last_layers = gr.Textbox(label='Stop at CLIP layers', value='1, 2, 1')
+		
+		return randomize_enabled, randomize_param_sampler_index, randomize_param_cfg_scale, randomize_param_steps, randomize_param_width, randomize_param_height, randomize_hires, randomize_hires_denoising_strength, randomize_hires_width, randomize_hires_height, randomize_other_CLIP_stop_at_last_layers
